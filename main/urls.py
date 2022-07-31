6965bc72ce53d536a89991906d7c749d96fdc34a
potlitel <potlitel@gmail.com>
2022-07-31 11:47:34 -0400

    Se incorpora navegación para las opciones "Dashboard" y "Products".

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ app_name = "main"
 
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
+    path("", views.homepage, name="home"),
+    path("/products", views.products, name="products"),
     ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
 	return render(request = request, template_name="home.html")
+
+def products(request):
+    return render(request = request, template_name="products.html")
